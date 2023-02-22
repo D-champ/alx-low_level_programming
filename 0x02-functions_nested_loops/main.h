a7b59ed85192cc43f29f3d71f34d93b2d2db4ddb
Oladele Daniel <danieloladele2000@gmail.com>
2023-02-22 11:30:33 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#ifndef main_h
+#ifndef mmain_h
 #define main_h
 
-char _putchar(char c);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,8 +11,8 @@ int _abs(int);
 int print_last_digit(int nld);
 void jack_bauer(void);
 void times_table(void);
-int add(int one, int two);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif /* main_h */
+#endif
